c2fa5bc733b7e887b76840cacb8c2ca5d8cc926d
Taemin Eom <umtaemin1125@naver.com>
2023-05-24 17:47:41 +0900

    feat:Comment 날짜 관리를 위한 EnableJpaAuditing 어노테이션 추가

--- back-end/src/main/java/com/petmily/backend/BackEndApplication.java
+++ back-end/src/main/java/com/petmily/backend/BackEndApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EntityScan(basePackages = "backend")
+@EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackEndApplication {
 
