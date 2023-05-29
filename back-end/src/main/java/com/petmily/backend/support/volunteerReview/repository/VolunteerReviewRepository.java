bb4aba0448c86480ead9457b75fd58df266c6c91
Taemin Eom <umtaemin1125@naver.com>
2023-05-29 18:12:18 +0900

    feat: 봉사 후기 게시판 페이지네이션 기능 추가

--- back-end/src/main/java/com/petmily/backend/support/volunteerReview/repository/VolunteerReviewRepository.java
+++ back-end/src/main/java/com/petmily/backend/support/volunteerReview/repository/VolunteerReviewRepository.java
@@ -1,9 +1,14 @@
 package com.petmily.backend.support.volunteerReview.repository;
 
 import com.petmily.backend.support.volunteerReview.domain.VolunteerReview;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VolunteerReviewRepository extends JpaRepository<VolunteerReview, Long> {
+    @NotNull
+    Page<VolunteerReview> findAll(@NotNull Pageable pageable);
 }
