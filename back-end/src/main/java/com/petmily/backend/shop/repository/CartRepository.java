1939fd8926518a628427cd0a1da65313885883f8
Taemin Eom <umtaemin1125@naver.com>
2023-06-05 10:33:19 +0900

    feat: 장바구니 조회, 삭제, 수량 증가 감소 구현

--- back-end/src/main/java/com/petmily/backend/shop/repository/CartRepository.java
+++ back-end/src/main/java/com/petmily/backend/shop/repository/CartRepository.java
@@ -1,18 +1,21 @@
-//package com.petmily.backend.shop.repository;
-//
-//import java.util.List;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.petmily.backend.member.login.domain.Member;
-//import com.petmily.backend.shop.domain.Cart;
-//import com.petmily.backend.shop.dto.CartDto;
-//
-//@Repository
-//public interface CartRepository extends JpaRepository<Cart,Long> {
-//		List<CartDto> findByMember(Member member);
-//
-//
-//	
-//}
+package com.petmily.backend.shop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.petmily.backend.member.login.domain.Member;
+import com.petmily.backend.shop.domain.Cart;
+import com.petmily.backend.shop.dto.CartDto;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart,Long> {
+    List<Cart> findByMember(Member member);
+    void deleteByMemberAndCartNum(Member member, Long cartNum);
+
+    @Transactional
+    void deleteByMemberAndCartNumIn(Member member, List<Long> cartNums);
+    Cart findByMemberAndCartNum(Member member, Long cartNum);
+}
