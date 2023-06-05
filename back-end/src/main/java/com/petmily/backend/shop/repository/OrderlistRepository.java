338810aaeaf68ed8802878405c3d0102bf2c0da7
Taemin Eom <umtaemin1125@naver.com>
2023-06-05 11:12:26 +0900

    fix : orderlist 주석처리

--- back-end/src/main/java/com/petmily/backend/shop/repository/OrderlistRepository.java
+++ back-end/src/main/java/com/petmily/backend/shop/repository/OrderlistRepository.java
@@ -2,24 +2,13 @@ package com.petmily.backend.shop.repository;
 
 import com.petmily.backend.shop.domain.Orderlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderlistRepository extends JpaRepository<Orderlist, Long> {
-
-// import java.util.List;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Modifying;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.data.repository.query.Param;
-// import org.springframework.stereotype.Repository;
-// import org.springframework.transaction.annotation.Transactional;
-
-// import com.petmily.backend.shop.domain.Orderlist;
-// import com.petmily.backend.shop.dto.OrderAdmin;
 
 // @Repository
 // public interface OrderlistRepository extends JpaRepository<Orderlist, Long> {
-	
 // 	@Query(nativeQuery=true, value="select o.orderNum, o.orderDate, o.orderState, o.boardNum, o.quantity, "
 // 			+ "o.memberNum, o.cost from orderlist o order by o.orderDate desc")
 // 	List<OrderAdmin> getOrderlist();
