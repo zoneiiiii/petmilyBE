077f2ca2035cf7e3877b0014661a6601bce3d49f
Taemin Eom <umtaemin1125@naver.com>
2023-06-06 20:24:42 +0900

    feat:Orderlist 분리한 테이블 데이터로 수정

--- back-end/src/main/java/com/petmily/backend/shop/domain/Orderlist.java
+++ back-end/src/main/java/com/petmily/backend/shop/domain/Orderlist.java
@@ -14,46 +14,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 
 public class Orderlist {
-	
-// 	@Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-// 	private long orderNum;
-	
-// 	@Column
-// 	private int memberNum;
-	
-// 	@Column
-// 	private int boardNum;
-
-// 	@Column
-// 	private DateTime orderDate;
-
-// 	@Column
-// 	private int quantity;
-
-// 	@Column
-// 	private int cost;
-
-// 	@Column
-// 	private String orderState;
-
-// 	@Column
-// 	private int postal;
-
-// 	@Column
-// 	private String address;
-
-// 	@Column
-// 	private String addressDetail;
-
-// 	@Column
-// 	private String note;
-
-// 	@Column
-// 	private String recipient;
-
-// 	@Column
-// 	private String recipientTel;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
